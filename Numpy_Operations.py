dd36fd7563bddd70dcef0cf22902cd0208cfb70b
Ritwik Baranwal <45766294+Vortez2471@users.noreply.github.com>
2019-12-13 14:42:02 +0530

    Add files via upload

---
+++ Numpy_Operations.py
@@ -0,0 +1,23 @@
+#ARITHMETIC
+
+import numpy as np
+
+arr=np.arange(0,10)
+
+print(arr+arr)
+print(arr*arr)
+print(arr-arr)
+print(arr**3)
+
+print(arr/arr)
+
+print(1/arr)
+
+# UNIVERSAL ARRAY FUNCTIONS
+
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.max(arr))
+print(np.sin(arr))
+print(np.log(arr))
+
